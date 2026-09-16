dae7538505270bbd7d9bb782166467fe4d6e736d
tm146796-arch <tm146796@continental.edu.pe>
2026-09-15 21:05:48 -0500

    configuracion inicial del proyecto

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "cordero.pa2.org"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "cordero.pa2.org"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
